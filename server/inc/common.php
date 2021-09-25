10b938b85870c5c348dd8d64207027495d367be5
Firmaplus(+) Ltd <w.stubig@firmaplus.de>
2021-09-25 16:14:38 +0200

    add function for random game detection

--- server/inc/common.php
+++ server/inc/common.php
@@ -13,3 +13,29 @@ function getLevelNrListByUserName($username, $base_path){
     }
     return $level_nr_list;
 }
+
+function isRandomGame($gn){
+    return ($gn == 3 || $gn == 4 || $gn > 6 );
+}
+
+function sendScoreToHSC($PlusStoreId, $score_array, $level){
+
+    $data = chr($level >> 16 & 0xFF).chr($level >>  8 & 0xFF).chr($level & 0xFF). // Level to 3 byte number
+            chr($score_array[0]).chr($score_array[1]).chr($score_array[2]).
+            chr(39); // C.A.V.E. Apocalypse game ID in HSC DB
+
+    $options = array (
+        'http' => array (
+            'method' => 'POST',
+            'header'=> "Content-Type: application/octet-stream\r\n".
+                       "Content-Length: " . strlen($data) . "\r\n".
+                       "PlusStore-ID: " . $PlusStoreId . "\r\n",
+            'content' => $data
+        )
+    );
+    $context  = stream_context_create($options);
+
+    file_get_contents("https://h.firmaplus.de/a.php", false, $context);
+
+
+}
